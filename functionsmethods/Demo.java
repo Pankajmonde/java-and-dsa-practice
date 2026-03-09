64dbc8229912b74bcfa25b6ac680e1d4bb72407b
Pankaj Monde <pankajmonde96@gmail.com>
2026-03-09 19:52:47 +0530

    functionsbasic

---
+++ functionsmethods/Demo.java
@@ -0,0 +1,15 @@
+package functionsmethods;
+
+public class Demo {
+     public static void printHello() {
+      System.out.println("Hello");
+      System.out.println("Hello");
+      System.out.println("Hello");
+
+      
+     }
+    public static void main(String[] args) {
+        printHello();  // calling the method //function call
+
+    }
+}
