3b77abc96e2f6b7a2275ad8052b1f4f44b5f603f
Pankaj Monde <pankajmonde96@gmail.com>
2026-08-14 23:31:24 +0530

    example

--- Alogorithms/Sorting/insertionSort.java
+++ Alogorithms/Sorting/insertionSort.java
@@ -1,6 +1,28 @@
 package Alogorithms.Sorting;
 
+import java.util.Arrays;
+
 public class insertionSort {
-    
-    
+     
+  public static void main(String args[]) {
+
+        int arr[] = {5, 4, 3, 2, 1};
+
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }   
+    public static void insertionSort(int []arr){
+        int n=arr.length;
+        for(int i=1; i<n; i++){
+            int key=arr[i];
+            int prev=i-1;
+
+            while(prev>=0 && arr[prev]>key){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=key;
+        }
+    }
 }
